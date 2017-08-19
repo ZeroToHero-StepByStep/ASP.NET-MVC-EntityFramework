a01501b3d4f1cb9cd00e78511ece08616137480f
Pei <jzha471@aucklanduni.ac.nz>
2017-08-19 22:58:22 +1200

    37. Data Annotation ( a startup project )

---
+++ BuildingModelWithCodeFirst/DataAnnotation/DataAnnotation/PlutoContext.cs
@@ -0,0 +1,38 @@
+namespace DataAnnotation
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Cours> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>()
+                .HasMany(e => e.Courses)
+                .WithOptional(e => e.Author)
+                .HasForeignKey(e => e.Author_Id);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(e => e.Courses)
+                .WithOptional(e => e.Category)
+                .HasForeignKey(e => e.Category_Id);
+
+            modelBuilder.Entity<Cours>()
+                .HasMany(e => e.Tags)
+                .WithMany(e => e.Courses)
+                .Map(m => m.ToTable("TagCourses").MapLeftKey("Course_Id"));
+        }
+    }
+}
