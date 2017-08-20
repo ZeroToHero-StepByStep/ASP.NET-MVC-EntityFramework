8a1509e00981ea85a45df17d9a4dd65884978a17
Pei <jzha471@aucklanduni.ac.nz>
2017-08-20 15:44:29 +1200

    40. Demo Fluent API ( a startup project )

---
+++ BuildingModelWithCodeFirst/Fluent API/Fluent API/Course.cs
@@ -0,0 +1,43 @@
+namespace DataAnnotation
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Courses")]
+    public partial class Course
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Course()
+        {
+            Tags = new HashSet<Tag>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        public int Level { get; set; }
+
+        public float FullPrice { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+
+        public int? Category_Id { get; set; }
+
+        public virtual Author Author { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ BuildingModelWithCodeFirst/Fluent API/Fluent API/PlutoContext.cs
@@ -0,0 +1,26 @@
+namespace DataAnnotation
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           
+
+        }
+    }
+}
